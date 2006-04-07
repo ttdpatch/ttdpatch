c8c6d20f31356784e0877de5e2eb4775b0817cfa
patchman <josef@ttdpatch.net>
2006-04-07 15:05:20 +0000

    moved start.bat to bin; copy it to main dir and edit there to use it

--- bin/start.bat
+++ bin/start.bat
@@ -1,4 +1,8 @@
 @echo off
+REM
+REM Don't edit or run this file here, copy it to the main source directory
+REM and use it from there (svn will ignore changes there, but not here)
+REM
 
 REM -- Use this if the Cygwin /bin directory is not in the PATH --
 rem PATH D:\Cygwin\bin;%PATH%
