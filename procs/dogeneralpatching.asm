d5ec051a93471ea227c75b5aef9c7b1cbd02d135
patchman <josef@ttdpatch.net>
2006-07-02 18:05:58 +0000

    allocate veh2 before all patching

--- procs/dogeneralpatching.asm
+++ procs/dogeneralpatching.asm
@@ -1418,6 +1418,11 @@ vard vehclassspecptr, trainpower_ptr, rvspeed_ptr, shipsprite_ptr, planesprite_p
 
 global dogeneralpatching
 dogeneralpatching:
+	imul eax,[newvehicles],byte veh2_size
+	push eax
+	call malloccrit
+	pop dword [veh2ptr]
+	// don't init here yet, it'll mess up searching...
 
 	// this variable is sometimes not initialized properly
 	// it should be the current mouse cursor, but sometimes the
@@ -2086,12 +2091,6 @@ dogeneralpatching:
 	storeaddress oldcopyoldorder,1,1,copyvehordersfn
 
 	multipatchcode oldshowvehstat,newshowvehstat,4
-
-	imul eax,[newvehicles],byte veh2_size
-	push eax
-	call malloccrit
-	pop dword [veh2ptr]
-	// don't init here yet, it'll mess up searching...
 
 	patchcode cleartile
 
