6dffb854842fc916e6f26de2b7b29f711788ecad
patchman <josef@ttdpatch.net>
2006-07-29 02:04:14 +0000

    merging 2.5 r698:r703

--- patches/signcht.asm
+++ patches/signcht.asm
@@ -1916,22 +1916,22 @@ landinfocheat:
 
 	// Please note that if a landscape isn't loaded it has a value of 0x80000000, not 0x0
 	mov edx,landscape6
-	cmp edx, 0x80000000
-	je .no_l8
+	test edx,edx
+	jle .no_l6
 	mov dl,[edx+esi]
 	call writehexbyte
 
 .no_l6:
 	mov edx, landscape7 // Adds L7 information to the sign
-	cmp edx, 0x80000000
-	je .no_l8
+	test edx,edx
+	jle .no_l7
 	mov dl, [edx+esi]
 	call writehexbyte
 
 .no_l7:
 	mov edx, landscape8 // Adds L8 information to the sign
-	cmp edx, 0x80000000
-	je .no_l8
+	test edx,edx
+	jle .no_l8
 	mov dx, [edx+esi*2]
 	xchg dh, dl
 	call writehexbyte
