df9afaf2eea0e042f49433ae7b0748b6a7ccbfb8
patchman <josef@ttdpatch.net>
2006-08-13 01:15:20 +0000

    fix base graphics not getting loaded unless another .grf was loaded as well

--- patches/grfload.asm
+++ patches/grfload.asm
@@ -102,6 +102,7 @@ proc initializegraphics
 
 	// process newgrf(w).cfg
 	call processnewgrf
+	testflags canmodifygraphics,bts
 
 .none:
 
@@ -562,7 +563,6 @@ proc readgrffile
 	jl .fail
 
 	add [totalnewsprites],esi
-	testflags canmodifygraphics,bts
 
 .fail:
 	and dword [curgrffile],0
