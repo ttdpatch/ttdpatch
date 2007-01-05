13fa87221110b21d48d6d77bcec6d1499669f002
DaleStan <dalestan@gmail.com>
2007-01-05 04:34:41 +0000

    Access the landscape arrays as arrays in C code.

--- inc/ttdvar.h
+++ inc/ttdvar.h
@@ -5,6 +5,10 @@
 #define ttdvar(a,b) asm (".set _" #a "," #b);
 #endif
 #define __C_SOURCE__
+
+#define landscape4 landscape4base // In C code, use of extern char landscape4[256][256]; is preferred.
+#define landscape5 landscape5base
+
 #include <ttdvar.inc>
 
 // to define their size and access them in C code, you still need something like
