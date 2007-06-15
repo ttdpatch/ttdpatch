8e9beb8c7c80bfd4682ea237cb5580d4eb400c9f
JGR <j.g.rennison@gmail.com>
2007-06-15 21:05:15 +0000

    Expand Oskar's increase vehicle consist code to support ships, aircraft and road vehicles as well. Not tested under DOS. Not tested with more than 240 aircraft or road vehicles.

--- patches/incvehconsists.asm
+++ patches/incvehconsists.asm
@@ -1,6 +1,6 @@
 // Even more vehicle support for TTD
 // by eis_os
-// currently supports only trains
+// aircraft, road and ship support added by JGR
 #include <std.inc>
 #include <veh.inc>
 #include <proc.inc>
@@ -12,7 +12,29 @@ global calctrainconsistnumber
 proc calctrainconsistnumber
 	slocal counter,byte,256
 	_enter
+	push ebx
 	push eax
+	mov ebx, testvehicle_train
+	jmp calctrainconsistnumber_start
+exported calcshipconsistnumber
+	_enter
+	push ebx
+	push eax
+	mov ebx, testvehicle_ship
+	jmp calctrainconsistnumber_start
+exported calcairconsistnumber
+	_enter
+	push ebx
+	push eax
+	mov ebx, testvehicle_air
+	jmp calctrainconsistnumber_start
+exported calcrvconsistnumber
+	_enter
+	push ebx
+	push eax
+	mov ebx, testvehicle_rv
+	//jmp calctrainconsistnumber_start
+calctrainconsistnumber_start:
 	xor eax, eax
 	xor dx, dx
 	
@@ -23,23 +45,8 @@ proc calctrainconsistnumber
 	pop ecx
 
 	mov edi, [veharrayptr]
-.testvehicle:
-	cmp byte [edi+veh.class], 0x10
-	jnz .nextveh
-	cmp byte [edi+veh.subclass], 0
-	jnz .nextveh
 
-	mov dl, byte [edi+veh.owner]
-	cmp dl, byte [curplayer]
-	jnz .nextveh
-
-	mov al, byte [edi+veh.consistnum]
-	inc byte [%$counter+eax]
-.nextveh:
-	sub edi,byte -vehiclesize	//add esi,vehiclesize
-	cmp edi,[veharrayendptr]
-	jb .testvehicle
-
+	call ebx
 
 	mov dl, 0xFF 
 .next:
@@ -59,10 +66,83 @@ proc calctrainconsistnumber
 	mov dl, al
 
 	pop eax
+	pop ebx
 	stc
 	_ret
 .fail:
 	pop eax
+	pop ebx
 	clc
 	_ret
+
+testvehicle_train:
+	cmp byte [edi+veh.class], 0x10
+	jnz .nextveh
+	cmp byte [edi+veh.subclass], 0
+	jnz .nextveh
+
+	mov dl, byte [edi+veh.owner]
+	cmp dl, byte [curplayer]
+	jnz .nextveh
+
+	mov al, byte [edi+veh.consistnum]
+	inc byte [%$counter+eax]
+.nextveh:
+	sub edi,byte -vehiclesize	//add esi,vehiclesize
+	cmp edi,[veharrayendptr]
+	jb testvehicle_train
+	ret
+	
+testvehicle_ship:
+	cmp byte [edi+veh.class], 0x12
+	jnz .nextveh
+
+	mov dl, byte [edi+veh.owner]
+	cmp dl, byte [curplayer]
+	jnz .nextveh
+
+	mov al, byte [edi+veh.consistnum]
+	inc byte [%$counter+eax]
+.nextveh:
+	sub edi,byte -vehiclesize	//add esi,vehiclesize
+	cmp edi,[veharrayendptr]
+	jb testvehicle_ship
+	ret
+	
+testvehicle_air:
+	cmp byte [edi+veh.class], 0x13
+	jnz .nextveh
+	cmp byte [edi+veh.subclass], 2
+	ja .nextveh
+
+	mov dl, byte [edi+veh.owner]
+	cmp dl, byte [curplayer]
+	jnz .nextveh
+
+	mov al, byte [edi+veh.consistnum]
+	inc byte [%$counter+eax]
+.nextveh:
+	sub edi,byte -vehiclesize	//add esi,vehiclesize
+	cmp edi,[veharrayendptr]
+	jb testvehicle_air
+	ret
+	
+testvehicle_rv:
+	cmp byte [edi+veh.class], 0x11
+	jnz .nextveh
+
+	mov dl, byte [edi+veh.owner]
+	cmp dl, byte [curplayer]
+	jnz .nextveh
+
+	mov al, byte [edi+veh.consistnum]
+	inc byte [%$counter+eax]
+.nextveh:
+	sub edi,byte -vehiclesize	//add esi,vehiclesize
+	cmp edi,[veharrayendptr]
+	jb testvehicle_rv
+	ret
+	
+
+
 endproc calctrainconsistnumber
--- procs/incvehconsists.asm
+++ procs/incvehconsists.asm
@@ -6,7 +6,7 @@ patchprocandor experimentalfeatures,BIT(EXP_INCVEHCONSIST),, patchincvehconsists
 
 begincodefragments
 
-codefragment oldcalctrainconnumber, -8
+codefragment oldcalctrainconnumber, -8	//SetupNewTrain, 005760D9, BuyRailVehicle, _CS:001640CC
 	cmp byte [edi+veh.class], 0x10
 	jnz $+2+0x16
 	cmp byte [edi+veh.subclass], 0
@@ -19,10 +19,51 @@ codefragment newcalctrainconnumber
 	ret
 .ok:
 	setfragmentsize 54
+	
+codefragment oldcalcshipconnumber, -8	//BuyShip, _CS:0016A45A
+	cmp byte [edi+veh.class], 0x12
+	jnz $+2+0x10
+	mov dh, [edi+veh.owner]
+codefragment newcalcshipconnumber
+	icall calcshipconsistnumber
+	jc .ok
+	mov ebx, 0x80000000
+	ret
+.ok:
+	setfragmentsize 48
+	
+codefragment oldcalcrvconnumber, -8	//BuyRoadVehicle, _CS:00167B28
+	cmp byte [edi+veh.class], 0x11
+	jnz $+2+0x10
+	mov dh, [edi+veh.owner]
+codefragment newcalcrvconnumber
+	icall calcrvconsistnumber
+	jc .ok
+	mov ebx, 0x80000000
+	ret
+.ok:
+	setfragmentsize 48
 
+codefragment oldcalcairconnumber, -8	//BuyAircraft, _CS:0016E758
+	cmp byte [edi+veh.class], 0x13
+	jnz $+2+0x16
+	cmp byte [edi+veh.subclass], 2
+	ja $+2+0x10
+	mov dh, [edi+veh.owner]
+codefragment newcalcairconnumber
+	icall calcairconsistnumber
+	jc .ok
+	mov ebx, 0x80000000
+	ret
+.ok:
+	setfragmentsize 54
+	
 endcodefragments
 
 patchincvehconsists:
 	patchcode calctrainconnumber
+	patchcode calcshipconnumber
+	patchcode calcrvconnumber
+	patchcode calcairconnumber
 	ret
 
